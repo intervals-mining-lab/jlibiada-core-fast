a8f89fe06012eefc095cee52497fca4a6a034a90
Alex Semenov <avs110@inbox.ru>
2012-01-20 03:22:01 +0700

    FastAlphabet tests are fixed

--- src/test/FastChainAlgoritms/FastChain/testFastAlphabet.java
+++ src/test/FastChainAlgoritms/FastChain/testFastAlphabet.java
@@ -10,10 +10,9 @@ import libiada.FastChainAlgorithms.FastChain.FastAlphabet;
  * Time: 1:13
  */
 public class testFastAlphabet extends TestCase {
-    private FastAlphabet alphabet;
+    private FastAlphabet alphabet = new FastAlphabet();
 
-    public void testAdd() throws Exception {
-        alphabet = new FastAlphabet();
+    public void testAddGet() throws Exception {
         alphabet.add("123");
         alphabet.add("11");
 
@@ -21,7 +20,48 @@ public class testFastAlphabet extends TestCase {
         assertEquals("11", alphabet.get(1));
     }
 
-    public void addNull() throws Exception {
+    public void testAddNull() throws Exception {
+        try {
+            alphabet.add(null);
+        } catch (Exception e) {
+            return;
+        }
+        fail();
+    }
 
+    public void testAddEmptyString() throws Exception {
+        try {
+            alphabet.add("");
+        } catch (Exception e) {
+            return;
+        }
+        fail();
+    }
+
+    public void testAddDuplicateValue() throws Exception {
+        alphabet.add("1");
+        try {
+            alphabet.add("1");
+        } catch (Exception e) {
+            return;
+        }
+        fail();
+    }
+
+    public void testIndexOf() throws Exception {
+        alphabet.add("1");
+        alphabet.add("2");
+        alphabet.add("12");
+
+        assertEquals(alphabet.indexOf("1"), 0);
+        assertEquals(alphabet.indexOf("2"), 1);
+        assertEquals(alphabet.indexOf("12"), 2);
+    }
+
+    public void testSize() throws Exception {
+        alphabet.add("1");
+        alphabet.add("2");
+        alphabet.add("12");
+        assertEquals(alphabet.size(), 3);
     }
 }
