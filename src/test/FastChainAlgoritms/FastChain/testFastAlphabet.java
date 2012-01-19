e43bc92e899eb948dd4bdbc09e6af29e189c89b8
Alex Semenov <avs110@inbox.ru>
2012-01-20 02:43:38 +0700

    cobertura bug fixed

--- src/test/FastChainAlgoritms/FastChain/testFastAlphabet.java
+++ src/test/FastChainAlgoritms/FastChain/testFastAlphabet.java
@@ -1,6 +1,7 @@
 package test.FastChainAlgoritms.FastChain;
 
 import junit.framework.TestCase;
+import libiada.FastChainAlgorithms.FastChain.FastAlphabet;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,5 +10,18 @@ import junit.framework.TestCase;
  * Time: 1:13
  */
 public class testFastAlphabet extends TestCase {
+    private FastAlphabet alphabet;
 
+    public void testAdd() throws Exception {
+        alphabet = new FastAlphabet();
+        alphabet.add("123");
+        alphabet.add("11");
+
+        assertEquals("123", alphabet.get(0));
+        assertEquals("11", alphabet.get(1));
+    }
+
+    public void addNull() throws Exception {
+
+    }
 }
